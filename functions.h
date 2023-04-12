3ea7af2e1d3f62b999279b9c071f68348ba2ec4e
RahgirRafi <rahgirrafi@gmail.com>
2023-04-12 17:22:08 +0600

    Fixed a minor issue

--- functions.h
+++ functions.h
@@ -1,1 +1,25 @@
 #pragma once
+#pragma once
+#include <iostream>
+using namespace std;
+
+struct Node {
+	int data;
+	Node* next;
+};
+
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+void insert();
+void insert(Node** head, int data);
+void insert(Node** head, int data, int n);
+void printList(Node* head);
+void remove(Node** head, int data);
+void removeNth(Node** head, int n);
+void reverseList(Node** head);
+
+void reverseListRecursive(Node** head);
+
+
+#endif FUNCTIONS_H
